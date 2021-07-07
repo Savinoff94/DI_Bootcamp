77e8dbb00aa415187d89568ae3b87c298c1e0eb4
Savinoff94 <savinoff94@mail.ru>
2021-07-07 10:04:40 +0300

    Week2 day 3

--- Week2/Day3/Daily_challenge/EX_1/script.js
+++ Week2/Day3/Daily_challenge/EX_1/script.js
@@ -1,5 +1,5 @@
-let str = "*";
-let str2 = "";
+//let str = "*";
+//let str2 = "";
 
 
 //the first way 
@@ -7,12 +7,20 @@ let str2 = "";
 //     console.log(str.repeat(i+1));
 // }
 
-//the second
+//the second way 
 for(let i = 0; i < 6; i++){
-    
-    for(let g = 0; g < 1; g++){
+    let str2 = "";
+    for(let g = 0; g <= i; g++){
         str2 = str2.concat("*");
     }
     console.log(str2);
 }
 
+//the third way 
+
+// for(let i = 0; i < 6; i++){
+//     for(let g = 0; g <= 1; g++){
+//         str2 = str2.concat("*");
+//     }
+//     console.log(str2);
+// }
